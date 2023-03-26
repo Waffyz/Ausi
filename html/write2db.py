431e70f52bec51140e592635feeae0460bd396ef
Barmp <Barmp@users.noreply.github.com>
2023-03-25 23:54:03 -0700

    Created pages to test simple DB read and write

---
+++ html/write2db.py
@@ -0,0 +1,12 @@
+####
+## Python app to accept data from one of several HTML form submissions and write 
+##    the data to a database. 
+##
+## --Should check which HTML page has invoked it (if possible)
+## --Should check data prior to writing to the database
+##      -database is strict about data types being entered?
+##      -some santiy checking of the data being submitted is a good idea.
+## -- Should fail if executed directly or not launched by one of the HTML pages.
+## -- It's ok for this to be highly insecure, it will be useful to break it later.
+####
+
